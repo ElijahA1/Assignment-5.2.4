2cb2b21ef82e58ba1bf5b6dfd516e6687cc97e6e
ElijahA1 <elijah.abourezk@outlook.com>
2025-08-05 14:03:08 -0700

    Add project files.

---
+++ Assignement 5.2.4/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Assignment_5._2._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(isPalindrome("RaDAr"));
+            Console.WriteLine(isPalindrome("RAdDar"));
+            Console.WriteLine(isPalindrome("sonar"));
+        }
+        static bool isPalindrome(string s) 
+        {
+            s = s.ToLower();
+
+            if (s.Length < 2) 
+            { 
+                return true;
+            }
+
+            if (s[0] != s[s.Length - 1]) 
+            {
+                return false;
+            }
+
+            string subString = s.Substring(1, s.Length-2);
+
+            if (isPalindrome(subString)) 
+            { 
+                return true; 
+            }
+            return false;
+
+        }
+    }
+}
